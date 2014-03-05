f333678ac1eb5d8a960d1cf1680f3d709f74341c
Peter Byriel <peter.byriel@gmail.com>
2014-03-05 22:07:23 +0100

    Completed project version1

---
+++ js/app.js
@@ -0,0 +1,32 @@
+var count = 0;
+
+$(document).ready(function () {
+    $("#count").text("No. items: " + count);
+    
+    $("#item").keydown(function (event) {
+        var uinput = $("#item").val();
+        if (event.keyCode == 13) {
+            if (uinput == "") {
+                alert("Please enter an item");
+            } else {
+                $("ul").prepend("<li class='todo'>" + uinput + "</li>");
+                $("#item").val("");
+                count += 1;
+                $("#count").text("No. items: " + count);
+        }
+        }
+    });
+
+    $("ul").on("click", "li", function () {
+        if ($(this).hasClass("todo")) {
+            $(this).removeClass("todo").addClass("done");
+            count -= 1;
+            $("#count").text("No. items: " + count);
+            $(this).appendTo("ul");
+        } else {
+            $(this).remove();
+        }
+    });
+    
+    
+});
